4a2ddcf08a6b8cee3b44da82b06488ad69a062b9
Phuong 'J' Le H <le.hoai.phuong@framgia.com>
2014-02-19 13:01:13 +0700

    init

---
+++ config/deploy/staging.rb
@@ -0,0 +1,55 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+# Don't declare `role :all`, it's a meta role
+# role :app, %w{deploy@example.com}
+# role :web, %w{deploy@example.com}
+# role :db,  %w{deploy@example.com}
+
+set :rails_env, "staging"
+set :deploy_env, "staging"
+
+# Directories
+set :deploy_to, "/usr/local/rails_apps/letscook"
+set :pid_file, "/tmp/unicorn_letscook_staging.pid"
+server "localhost", user: "deploy", roles: %W{web app db}, primary: true
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a hash can be used to set
+# extended properties on the server.
+# server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+
+# Your HTTP server, Apache/etc
+# role :web do servers end
+# This may be the same as your `Web` server
+# role :app do servers end
+# This is where Rails migrations will run
+# role :db, primary: true do servers end
+# role :db,  "your slave db-server here"
+
+# you can set custom ssh options
+# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
+# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
+# set it globally
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+# and/or per server
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
+# setting per server overrides global ssh_options
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4Capistrano3::Application.config.secret_key_base = 'c623315f1c0aa2fff1663efe48faec52445dd083be4a531362ee3076ed22c72f3159e4aa1a8326cbceff661da3f97e51502453db7a7a7bd23f7112766f60e97a'
